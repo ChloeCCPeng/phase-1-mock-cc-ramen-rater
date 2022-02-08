3f59ad7f1df464f8e6328db07d1e59af9ae2b075
Chloe Peng <chloepeng331@icloud.com>
2022-02-08 12:29:33 -0500

    commit tree 56800271331fe555ba905b84afc1832f089f5ee4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', function() {
+    function fetchRamens() {
+        fetch ('http://localhost:3000/ramens')
+        .then (Response =>Response.json())
+        .then (data => renderRamens(data))
+    }
+
+    function renderRamens(ramens){
+        console.log(ramens);
+    }
+
+    fetchRamens();
+});
